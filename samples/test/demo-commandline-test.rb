11f2c2cf156f4bfd594133cd283d05acf75e329e
Bjørn Stachmann <bstachmann@yahoo.de>
2012-11-23 22:48:02 +0100

    fix: erroneous output in unit test

--- samples/test/demo-commandline-test.rb
+++ samples/test/demo-commandline-test.rb
@@ -10,7 +10,7 @@ class DemoCommandlineTest < Test::Unit::TestCase
 
 	def test_creates_working_dir
 		`rm -rf sample1`
-		DemoCommandline.new do
+		DemoCommandline.new(@renderer) do
 			sh 'touch moin'
 		end
 		assert_equal "moin\n", `ls sample1`
@@ -18,33 +18,33 @@ class DemoCommandlineTest < Test::Unit::TestCase
 
 	def test_overwrites_existing_working_dir
 		`mkdir -p sample1; touch sample1/alt`
-		DemoCommandline.new {}
+		DemoCommandline.new(@renderer) {}
 		assert_equal "", `ls sample1`
 	end
 
 	def test_edit_new_file_with_content
-		DemoCommandline.new do
+		DemoCommandline.new(@renderer) do
 			edit 'wurst', :content =>"hallowelt\n", :commit => false
 		end
 		assert_equal "hallowelt\n", `cat sample1/wurst` 
 	end
 
 	def test_edit_new_file_with_default_content
-		DemoCommandline.new do
+		DemoCommandline.new(@renderer) do
 			edit 'kaese', :commit => false
 		end
 		assert_equal "0: Created file kaese lines [] (edit nr. 1)\n", `cat sample1/kaese` 
 	end
 
 	def test_edit_new_file_at_specified_lines
-		DemoCommandline.new do
+		DemoCommandline.new(@renderer) do
 			edit 'kaese', :line_numbers => [1,3], :commit => false
 		end
 		assert_equal "\n1: Created file kaese lines [1, 3] (edit nr. 1)\n\n3: Created file kaese lines [1, 3] (edit nr. 1)\n", `cat sample1/kaese` 
 	end
 
 	def test_edit_existing_file
-		DemoCommandline.new do
+		DemoCommandline.new(@renderer) do
 			File.open("sample1/kaese","w") { |f| f.write "a\nb\nc\n" }
 			edit 'kaese', :line_numbers => [1], :commit => false
 			edit 'kaese', :line_numbers => [2], :commit => false
