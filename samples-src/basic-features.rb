bdd909264e38e6ab41741baa598e08ad884768bf
Bjørn Stachmann <bstachmann@yahoo.de>
2013-12-19 18:46:40 +0100

    Edit samples with basic features

--- samples-src/basic-features.rb
+++ samples-src/basic-features.rb
@@ -2,11 +2,25 @@
 
 @demo = GitDemos.new('tmp/demo')
 @demo.section do
-	new_repo 'my-little-repo'
-	cd 'my-little-repo'
-	shell 'touch wurst'
-	markdown '## Und hier so Sachen machen'
-	shell 'ls -lah'
-	shell 'echo MOIN MOIN MOIN'
+
+	markdown <<-EOS
+## Markdown
+
+You can add *Markown*.
+	EOS
+
+	shell 'echo Hello World!'
+
+	new_repo 'repo'
+
+	cd 'repo'
+
+	create_file 'wurst'
+
+	shell 'git add --all'
+	shell 'git commit -m "first commit"'
+	shell 'git log --oneline'
+
+
 end
 
