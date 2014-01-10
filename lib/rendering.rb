34f195cfad7cd9d46b4739103ce106e9c11b7bf8
Bjørn Stachmann <bstachmann@yahoo.de>
2014-01-10 19:19:47 +0100

    refactor rendering

--- lib/rendering.rb
+++ lib/rendering.rb
@@ -3,19 +3,13 @@ module Rendering
 	def to_markdown file = nil
 		s = ""
 		@log.each do |entry|
-			s << entry[:text] << "\n" if entry[:text]
-
-			s << "    # " << entry[:desc] << "\n" if entry[:desc]
-
-			if entry[:shell] && !entry[:shell].empty?
-				entry[:shell].each { |cmd| s << "    " << cmd << "\n" }
-				if entry[:out]
-					entry[:out].each { |outputline| s << "    " << outputline << "\n" }
-				end
-			end
+			render_text(s, entry) if entry[:text]
+			render_desc(s, entry) if entry[:desc]
+			render_shell(s, entry) if entry[:shell]
+			render_out(s, entry) if entry[:out]
 			s << "\n"
 		end
-		
+	
 		if file
 			maruku = Maruku.new(s)
 			html = maruku.to_html
@@ -23,5 +17,22 @@ module Rendering
 		end
 		s
 	end
+
+	def render_text(s, entry)
+		s << entry[:text] << "\n" 
+	end
+
+	def render_desc(s, entry)
+		s << "    # " << entry[:desc] << "\n" 
+	end
+
+	def render_shell(s, entry)
+		entry[:shell].each { |cmd| s << "    " << cmd << "\n" }
+	end
+
+	def render_out(s, entry)
+		entry[:out].each { |outputline| s << "    " << outputline << "\n" } 
+	end
+
 
 end
