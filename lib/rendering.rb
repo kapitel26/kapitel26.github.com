1f1317733bd6abcd2a98fec704db9b445e0a2968
Bjørn Stachmann <bstachmann@yahoo.de>
2014-01-16 18:29:42 +0100

    refactor: use hash of blocks to make rendering more flexible

--- lib/rendering.rb
+++ lib/rendering.rb
@@ -1,12 +1,19 @@
 module Rendering
 
+	TYPES = [:text, :desc, :shell, :out]
+
 	def to_markdown file = nil
+		@renderers ||= {
+			text: lambda { |s, data| s << data << "\n"  },
+			desc: lambda { |s, data| s << "    # " << data << "\n"  },
+			shell: lambda { |s, data| data.each { |cmd| s << "    " << cmd << "\n" } },
+			out: lambda { |s, data| data.each { |outputline| s << "    " << outputline << "\n" }  }
+		}
+
 		s = ""
 		@log.each do |entry|
-			render_text(s, entry) if entry[:text]
-			render_desc(s, entry) if entry[:desc]
-			render_shell(s, entry) if entry[:shell]
-			render_out(s, entry) if entry[:out]
+			TYPES.reject { |type| entry[type].nil? }.
+				map { |type| @renderers[type].call(s,entry[type]) }
 		end
 	
 		if file
@@ -16,22 +23,5 @@ module Rendering
 		end
 		s
 	end
-
-	def render_text(s, entry)
-		s << entry[:text] << "\n" 
-	end
-
-	def render_desc(s, entry)
-		s << "    # " << entry[:desc] << "\n" 
-	end
-
-	def render_shell(s, entry)
-		entry[:shell].each { |cmd| s << "    " << cmd << "\n" }
-	end
-
-	def render_out(s, entry)
-		entry[:out].each { |outputline| s << "    " << outputline << "\n" } 
-	end
-
 
 end
