c470b884a190cac49f5b75279ed68a4669c1d2a7
Bjørn Stachmann <bstachmann@yahoo.de>
2014-01-17 00:22:29 +0100

    refactor: extract_file_writing

--- lib/rendering.rb
+++ lib/rendering.rb
@@ -8,18 +8,16 @@ module Rendering
 			out: lambda { |output, data| data.each { |outputline| output << "    " << outputline << "\n" }  }
 	}
 
-	def to_markdown file = nil
+	def to_markdown to_html_file = nil
 		
 		out = ""
-		
+
 		@log.each do |entry|
 			RENDERERS.each_pair { |type, renderer| renderer[out, entry[type]] if entry[type] }
 		end
 	
-		if file
-			maruku = Maruku.new(out)
-			html = maruku.to_html
-			File.write(file, html)
+		if to_html_file
+			File.write(to_html_file, Maruku.new(out).to_html)
 		end
 
 		out
