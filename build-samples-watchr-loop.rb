3bd16daa310e37c8a4329fb7a919739f25aa5750
Bjørn Stachmann <bstachmann@yahoo.de>
2014-02-25 19:00:40 +0100

    noch 'ne externe änderung

--- build-samples-watchr-loop.rb
+++ build-samples-watchr-loop.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env watchr
+# Noch 'ne externe Änderung
 # Eine erste Änderung im externen Projekt
 # Eine Änderung im großen Projekt
 watch( 'git-demos/.*.rb$' ) { |md| system 'ccdd --run-tests "ruby git-demos/build-samples.rb"' }
