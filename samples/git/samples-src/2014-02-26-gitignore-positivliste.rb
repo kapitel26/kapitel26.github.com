9738b4f1a016b29addace56dd4a7a113d996ec76
Bjørn Stachmann <bstachmann@yahoo.de>
2014-03-05 08:49:24 +0100

    beitrag über positivlisten überarbeitet

--- samples/git/samples-src/2014-02-26-gitignore-positivliste.rb
+++ samples/git/samples-src/2014-02-26-gitignore-positivliste.rb
@@ -12,8 +12,6 @@ def gitignore content
 	__
 end
 
-
-
 @demo.section do
 
 	text <<-__
@@ -31,11 +29,11 @@ author: bst
 `.gitignore` als Whitelist, geht das?
 =====================================
 
-Die `.gitignore` ist eine Blacklist (Negativliste), d. h. sie gibt an,
+`.gitignore` ist eine Blacklist (Negativliste), d. h. sie gibt an,
 welche Dateien man von der Versionierung *ausschließen* möchte. 
-Möchte man aber nur wenige Verzeichnisse in einer großen
+Wenn man aber nur wenige Verzeichnisse in einer großen
 Verzeichnisbaum mit Git versionieren möchte, 
-wäre eine Whitelist praktischer. Geht das?
+dann wäre eine Whitelist praktischer. Geht das auch?
 
 	__
 	hide_all do
@@ -55,26 +53,23 @@ wäre eine Whitelist praktischer. Geht das?
 
 
 	text <<-__
-
 Kurze Antwort
 -------------
 
 **Ja**, aber es ist ein klein wenig umständlich.
-
-Das Beispiel `.gitignore`-Datei, für ein Projekt,
-das nur die Verzeichnisse `bob/` und `ada/sub/sub/`
-versionieren möchte.
+Die folgende `.gitignore`-Datei zeigt, wie
+es geht:
 	__
 
 	gitignore <<-__
-# Erstmal alle Top-Level-Verzeichnisse ignorieren
+# Erstmal alles ignorieren
 *
 
-# Top-Level-Verzeichnis a1 "un-ignorieren"
+# Verzeichnis /a1/ "un-ignorieren"
 !/a1/
 !/a1/**
 
-# Verzeichnis b1/b2/b3 schrittweise "un-ignorieren"
+# Verzeichnis /b1/b2/b3/ "un-ignorieren"
 !/b1/
 !/b1/b2/
 !/b1/b2/b3/
@@ -93,18 +88,19 @@ Tatsächlich ist die Sache überraschend kompliziert.
 Oft gibt es in einem Projekt Dateien oder Verzeichnisse, die man
 nicht versionieren möchte, z. B. generierte Dateien, Editor-Backups
 oder lokale Konfigurationen. 
-Mit Hilfe der `.gitignore`-Datei kann Dateien ignorieren lassen. 
-Diese werde dann bei `git status` nicht angezeigt 
+Mit Hilfe der `.gitignore`-Datei kann solche Dateien ignorieren lassen. 
+Diese werde dann mit `git status` nicht mehr angezeigt 
 und bei `git add -all` nicht hinzugefügt.
 
 Manchmal gibt es aber auch die Situation, dass man eine große Verzeichnisstruktur
 vorliegen hat, davon aber nur einige wenige Dateien und Verzeichnisse
-versionieren möchte. Zum Beispiel: Ein `/etc`-Verzeichnis enthält Konfigurationen
+versionieren möchte. Zum Beispiel: Ein `/etc/`-Verzeichnis enthält Konfigurationen
 für viele Tools. Einige davon sind für mein Projekt relevant und nur Diese
 möchte ich versionieren. 
 Mit Git ist das (natürlich) auch möglich.
 Aber es gibt ein paar kleine Fallstricke dabei.
-Wir betrachteh ein Beispiel:
+
+### Beispiel
 	__
 
 	hide_all { gitignore "" }
@@ -112,14 +108,14 @@ Wir betrachteh ein Beispiel:
 	shell 'git status --short --untracked-files=all'
 
  	text <<-__
-Wir wollen nur Dateien unterhalb von  `a1` und `/b1/b2/b3` versionieren.
-Man könnte für jedes unerwünschte Verzeichnis eine Zeile in die `.gitignore` eintragen,
+Wir wollen nur Dateien unterhalb von  `/a1/` und `/b1/b2/b3/` versionieren.
+Man könnte jedes unerwünschte Verzeichnis in die `.gitignore` eintragen,
 dann bliebe übrig, was man möchte.
 Bei großen Projekten kann das aber mühsam sein und müsste auch ständig
 nachgepflegt werden, wenn neue Verzeichnisse hinzukommen.
 
-Stattdessen können einfach mit einem beherzten `*` alles ausschließen, 
-um dann später, das was man möchte, wieder hinzuzufügen.
+Stattdessen können einfach mit einem beherzten `*` erstmal alles ausschließen, 
+um dann später Verzeichnisse, die man versioneren möchte, zu *un-ignorieren*.
  	__
 
 gitignore <<-__
@@ -137,12 +133,12 @@ denn `git add -f` sticht `.gitignore`
 	end
 	text <<-__
 Der Nachteil dabei: 
-Das Verzeichnis `b1/b2/b3`
-bleibt weiterhin unbeobachtet. `git status`zeigt nicht, 
-wenn dort noch unversionierte Dateien liegen.
+Das Verzeichnis `/b1/b2/b3/`
+bleibt weiterhin unbeobachtet. `git status` zeigt beispielsweise nicht, 
+wenn dort noch neue unversionierte Dateien auftauchen.
 
 Mit vorangestelltem `!` können ausgeblendete 
-Dateien und Verzeichnisse "reaktiviert" werden. Also:
+Dateien und Verzeichnisse laut Manual-Page *un-ignoriert* werden:
  	__
 	gitignore <<-__
 *
@@ -150,17 +146,17 @@ Dateien und Verzeichnisse "reaktiviert" werden. Also:
  	__
 	shell 'git status --short --untracked-files=all'
  	text <<-__
-WTF! War um ist `/b1/b2/b3` nicht aktiv geworden?
-Laut man-Page wirkt `!` auf Pfade,
+WTF! Warum ist `/b1/b2/b3/` nicht wieder aktiv geworden?
+Laut Dokumentation wirkt `!` auf Pfade,
 die in vorigen Zeilen ausgeblendet wurden.
-Es wurde doch in der Zeile mit `*` ausgeblendet, oder?
+Das Verzeichnis `/b1/b2/b3/` wurde doch in der Zeile mit dem  `*` ausgeblendet, oder?
 Warum ist sie dann jetzt nicht wieder sichtbar?
-Tatsächlich wurde durch `*` das Verzeichnis `b1` ausgeschlossen.
+Tatsächlich wurde durch `*` das Verzeichnis `/b1/` ausgeschlossen.
 Die darunter liegenden Verzeichnisse wurden nur implizit mit ausgeschlossen.
 
 Was wir tun können, ist: Zuerst mit `/b1/` das Verzeichnis
 wieder zurückholen. Und dann mit `/b1/**` dafür sorgen,
-dass auch alle Dateien darunter wieder sichtbar werden 
+dass auch alle Dateien und Verzeichnisse darunter wieder sichtbar werden 
 (diese waren ja ebenfalls durch `*` ausgeschlossen)
  	__
 	gitignore <<-__
@@ -170,8 +166,7 @@ dass auch alle Dateien darunter wieder sichtbar werden
  	__
 	shell 'git status --short --untracked-files=all'
  	text <<-__
-Jetzt sind alle Dateien unterhalb von `b1/` wieder drin!
-
+Jetzt sind alle Dateien unterhalb von `/b1/` wieder drin!
 Und das kann man beliebig tief fortsetzen. 
 Weil's so schön war ;-) gleich nochmal:
 	__
@@ -182,12 +177,15 @@ Weil's so schön war ;-) gleich nochmal:
 !/b1/b2/
 !/b1/b2/b3/
 !/b1/b2/b3/**
-	__
+			__
 	shell 'git status --short --untracked-files=all'
- 	text <<-__
+
+	text <<-__
+Ein wenig seltsam, aber es funktioniert. Typisch Git eben ;-)
+
 ### Wozu die vielen Slashes?
 
-In unserem Beispiel würde es ohne Slashes am Anfang und Ende der Pfade.
+In unserem Beispiel würde es auch ohne Slashes am Anfang und Ende der Pfade gehen.
 	__
 
 	gitignore <<-__
@@ -199,14 +197,14 @@ In unserem Beispiel würde es ohne Slashes am Anfang und Ende der Pfade.
 	__
 
 	text <<-__
-	`b1/b1`
+Ganz korrekt wäre das aber nicht, denn Slashes
+am Anfang sorgen dafür, dass nur Pfade die so beginnen
+*matchen*. Slashes am Ende sorgen dafür, dass nur
+Verzeichnisse *matchen*.
 	__
-	create "b1/b1"
+	create "b1/b1/b1/b1"
 	shell 'git status --short --untracked-files=all'
 
-	text <<-__
-Ein wenig seltsam, aber es funktioniert. Typisch Git eben ;-)
-	__
 
  
 
